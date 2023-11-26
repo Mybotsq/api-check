7d19827d1bca92e3376ee6aa31ff7b009c0ac6fb
fitwist <ekapatsa@infostart.ru>
2023-11-26 12:30:03 +0300

    Добавлен get-метод

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import datetime
+from datetime import timedelta
 
 app = FastAPI()
+
+# today = datetime.today().date()
+# yesterday = today - timedelta(hours=3)
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/get")
+async def get_time():
+    return {"time": datetime.datetime.now() - timedelta(hours=3, minutes=0)}
